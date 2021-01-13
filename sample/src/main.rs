5d097de5b24add9854201fb57e8aa336eed63701
Gabriel Simmer <gabriel@gitgalaxy.com>
2021-01-13 15:10:33 +0000

    Fix type typo.
    Linting/shellcheck fixes.
    
    Correct job name
    
    Add renamed command.
    
    Tweaking tests.
    
    Fix gitignore.
    
    Fix missing parent dir.
    
    Add yes confirmation to install.
    
    Remove pipe.
    
    Source cargo env.
    
    Exclude SC1090.
    
    Fix dir reorg.
    
    latest -> stable.
    
    Fix example for clippy.
    
    Added caching.
    
    Flag for using cache.
    
    Remove orb file.

--- sample/src/main.rs
+++ sample/src/main.rs
@@ -2,7 +2,8 @@
 mod tests {
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+    	let add = 1 + 2;
+        assert_eq!(add, 3);
     }
 }
 
--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,6 +1,10 @@
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o rustup.sh
+chmod +x rustup.sh
+./rustup.sh -y
 
-if [ $RUST_VERSION != "latest" ]; then
-	rustup toolchain install $RUST_VERSION
-	rustup default $RUST_VERSION
+source "$HOME"/.cargo/env
+
+if [ "$RUST_VERSION" != "stable" ]; then
+	rustup toolchain install "$RUST_VERSION" -y
+	rustup default "$RUST_VERSION" -y
 fi
