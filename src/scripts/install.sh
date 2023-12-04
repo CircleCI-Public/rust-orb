8b90cafd8dfa0685367e232c6cc2f0a2e01d9f41
Simon Sapin <simon.sapin@exyr.org>
2023-12-04 12:44:00 +0100

    Don’t leave rustup.sh behind or install an unused toolchain
    * This script would let rustup default to installing a `stable` toolchain, then install a different one if needed. Instead it now tells the bootstrap script which toolchain to install in the first place
    
    * Piping `curl` to `sh` avoids leaving a `rustup.sh` file behind. This could disturb some tools, for example `cargo publish` fails with:
    
    > to proceed despite this and include the uncommitted changes, pass the `--allow-dirty` flag

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o rustup.sh
-chmod +x rustup.sh
-./rustup.sh -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- \
+  -y --default-toolchain "$RUST_VERSION"
 
 # shellcheck source=/dev/null
 source "$HOME"/.cargo/env
-
-if [ "$RUST_VERSION" != "stable" ]; then
-	rustup toolchain install "$RUST_VERSION"
-	rustup default "$RUST_VERSION"
-fi
